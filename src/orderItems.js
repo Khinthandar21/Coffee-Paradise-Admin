76978e526c274b95ed33b33ecea6aafc84ebcb0a
Khinthandar21 <khineyeikmon@gmail.com>
2023-12-23 20:15:15 +0630

    final admin project

--- src/orderItems.js
+++ src/orderItems.js
@@ -3,13 +3,7 @@ export const ORDERS = [
     id: 1,
     name: "Espresso",
     price: 2.99,
-    qty: 1,
-  },
-  {
-    id: 2,
-    name: "Cappuccino",
-    price: 3.41,
-    qty: 1,
+    qty: 2,
   },
   {
     id: 3,
@@ -18,40 +12,10 @@ export const ORDERS = [
     qty: 1,
   },
   {
-    id: 4,
-    name: "Mocha",
-    price: 2.49,
-    qty: 1,
-  },
-  {
-    id: 5,
-    name: "Americano",
-    price: 2.79,
-    qty: 1,
-  },
-  {
     id: 6,
     name: "Milk Tea",
     price: 2.19,
-    qty: 1,
-  },
-  {
-    id: 7,
-    name: "Green Tea",
-    price: 3.19,
-    qty: 1,
-  },
-  {
-    id: 8,
-    name: "Milk",
-    price: 1.59,
-    qty: 1,
-  },
-  {
-    id: 9,
-    name: "Caramel Macchiato",
-    price: 4.65,
-    qty: 1,
+    qty: 3,
   },
   {
     id: 10,
